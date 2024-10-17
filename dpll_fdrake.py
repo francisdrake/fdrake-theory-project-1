84378a507e56fb4c8664614f4bed2ebe7264de5a
Francis Drake <fdrake@snd.edu>
2024-10-16 23:02:26 -0400

    function layout

--- dpll_fdrake.py
+++ dpll_fdrake.py
@@ -17,3 +17,43 @@
 #     l ← choose-literal(Φ);
 #     return DPLL(Φ ∧ {l}) or DPLL(Φ ∧ {¬l});
 
+
+def dppl(set_of_clauses):
+    Satisfiable = False
+
+    return Satisfiable
+
+def unit_propagate():
+    pass
+
+def pure_literal_assignment():
+    pass
+
+def check(Wff,Nvars,Nclauses,Assignment):
+# Run thru all possibilities for assignments to wff
+# Starting at a given Assignment (typically array of Nvars+1 0's)
+# At each iteration the assignment is "incremented" to next possible
+# At the 2^Nvars+1'st iteration, stop - tried all assignments
+    Satisfiable=False
+    while (Assignment[Nvars+1]==0):
+        # Iterate thru clauses, quit if not satisfiable
+        for i in range(0,Nclauses): #Check i'th clause
+            Clause=Wff[i]
+            Satisfiable=False
+            for j in range(0,len(Clause)): # check each literal
+                Literal=Clause[j]
+                if Literal>0: Lit=1
+                else: Lit=0
+                VarValue=Assignment[abs(Literal)] # look up literal's value
+                if Lit==VarValue:
+                    Satisfiable=True
+                    break
+            if Satisfiable==False: break
+        if Satisfiable==True: break # exit if found a satisfying assignment
+        # Last try did not satisfy; generate next assignment)
+        for i in range(1,Nvars+2):
+            if Assignment[i]==0:
+                Assignment[i]=1
+                break
+            else: Assignment[i]=0
+    return Satisfiable
